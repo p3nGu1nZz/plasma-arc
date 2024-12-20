6d79502f74c38ac9f4326822c10eea3c1c03ca2e
p3nGu1nZz <rawsonkara@gmail.com>
2024-12-20 13:29:08 -0500

    fix(server): update static file serving path
    - Serve static files from the correct build/space directory
    - Remove unnecessary static file serving paths
    - Ensure index.html is served for all other routes to avoid 404 errors

--- scripts/server.js
+++ scripts/server.js
@@ -1,25 +1,7 @@
 // scripts/server.js
 
-/**
- * @file server.js
- * @description Node.js server setup for serving static files and handling requests for the Plasma Arc Project.
- *              This script sets up middleware for logging requests, handling MIME types, and serving static assets.
- * @version 1.0.0
- * @license MIT
- * @see {@link https://github.com/p3nGu1nZz/plasma-arc|GitHub Repository}
- * @contact rawsonkara@gmail.com
- * 
- * Middleware:
- * - Logging requests using chalk for color-coded console output
- * - Setting correct MIME types for .js files
- * 
- * Static Files:
- * - Serving files from 'public', 'src', 'assets', 'components', 'shaders', 'utils', and 'wgpu' directories
- */
-
 import express from 'express';
 import path from 'path';
-import fs from 'fs';
 import { fileURLToPath } from 'url';
 import chalk from 'chalk';
 
@@ -29,13 +11,11 @@ const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-// Middleware to log requests
 app.use((req, res, next) => {
     console.log(chalk.blue(`Request URL: ${req.url}`));
     next();
 });
 
-// Middleware to set correct MIME types for .js files and log responses
 app.use((req, res, next) => {
     if (req.path.endsWith('.js')) {
         res.type('application/javascript');
@@ -44,16 +24,8 @@ app.use((req, res, next) => {
     next();
 });
 
-// Serve static files from the 'public', 'src', and 'assets' directories
-app.use(express.static(path.join(__dirname, '../assets')));
-app.use(express.static(path.join(__dirname, '../public')));
-app.use(express.static(path.join(__dirname, '../src')));
-app.use(express.static(path.join(__dirname, '../src/components')));
-app.use(express.static(path.join(__dirname, '../src/shaders')));
-app.use(express.static(path.join(__dirname, '../src/utils')));
-app.use(express.static(path.join(__dirname, '../src/wgpu')));
+app.use(express.static(path.join(__dirname, '../build/space')));
 
-// If no file is found, respond with a 404 error
 app.use((req, res, next) => {
     if (!res.headersSent) {
         res.status(404).send('404 Not Found');
